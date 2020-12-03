b9afd3e574a77430f14403e9cdc684fe1b56f694
Bruno Domingues <bruno_signorelli@outlook.com>
2020-12-03 11:07:50 -0300

    Fix django to cd into repo tasks in order to run install.sh

--- scripts/django.sh
+++ scripts/django.sh
@@ -2,6 +2,7 @@
 sudo apt update
 git clone https://github.com/BrunoSDomingues/tasks.git && mv tasks /home/ubuntu
 sudo sed -i 's/node1/ADD_IP_HERE/' /home/ubuntu/tasks/portfolio/settings.py 
-/home/ubuntu/tasks/./install.sh
+cd /home/ubuntu/tasks/
+./install.sh
 echo $? >> /home/ubuntu/default.txt
 reboot
